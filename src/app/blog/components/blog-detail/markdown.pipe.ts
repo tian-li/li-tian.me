bb30702a429a300e83ff130c09aa10ced885a76b
Tian <litiansq89@gmail.com>
2018-09-20 20:00:50 -0400

    resolve sanitizing HTML stripped some content warning

--- src/app/blog/components/blog-detail/markdown.pipe.ts
+++ src/app/blog/components/blog-detail/markdown.pipe.ts
@@ -1,16 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import * as marked from 'marked';
 
-@Pipe({name: 'markdown'})
+@Pipe({ name: 'markdown' })
 export class MarkdownPipe implements PipeTransform {
-  constructor(private sanitized: DomSanitizer) {}
-  transform(content: string): string {
-    console.log('markdown pipe', content);
-    // let v = this.sanitized.bypassSecurityTrustHtml(content)
-    // console.log('v', v);
-    let res = marked(content, { sanitize: true });
-    console.log('res', res);
-    return res;
+  constructor(private sanitized: DomSanitizer) { }
+  transform(content: string): SafeHtml {
+    return this.sanitized.bypassSecurityTrustHtml(marked(content, { sanitize: true }));
   }
 }
