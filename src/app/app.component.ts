24def48de1457f6777dc0c23ebec81651318d7be
Li, Tian <litiansq89@gmail.com>
2020-01-02 14:35:13 -0500

    基本好了

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,69 +1,21 @@
-import { Component, ViewChild, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { MatSidenav } from '@angular/material/sidenav';
-
-import { ScreenConfig } from './shared/models/screen-config';
+import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnDestroy {
-  @ViewChild(MatSidenav, {static: false})
-  sidenav: MatSidenav;
+export class AppComponent {
 
-  watcher: Subscription;
-  activeMediaQuery: string;
-  screenConfig: ScreenConfig = {
-    isMobile: false,
-    sidenavOpened: true,
-    sidenavMode: 'side',
-  };
-
-  // useNavBar = false;
+  isDesktop = true;
 
   constructor(breakpointObserver: BreakpointObserver) {
-    // breakpointObserver
-    //   .observe([Breakpoints.Handset, Breakpoints.TabletPortrait])
-    //   .subscribe((result: BreakpointState) => {
-    //     console.log("match result", result);
-
-    //     if(result.matches) {
-    //       this.useNavBar = true
-    //     }
-    //   });
-  }
-
-
-
-  toggleSideNav(): void {
-    this.sidenav.toggle();
-  }
-
-  onRouteChanged(): void {
-    if (this.screenConfig.isMobile) {
-      this.sidenav.close();
-    }
-  }
-
-  get mobileConfig(): ScreenConfig {
-    return {
-      isMobile: true,
-      sidenavOpened: false,
-      sidenavMode: 'push',
-    };
-  }
-
-  get desktopConfig(): ScreenConfig {
-    return {
-      isMobile: false,
-      sidenavOpened: true,
-      sidenavMode: 'side',
-    };
-  }
-
-  ngOnDestroy(): void {
-    this.watcher.unsubscribe();
+    breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.TabletPortrait])
+      .subscribe((result: BreakpointState) => {
+        console.log("match result", result);
+        this.isDesktop = !result.matches;
+      });
   }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { NavComponent } from "./components/nav/nav.component";
 import { RouterModule } from "@angular/router";
 import { ScreenSizeService } from "./services/screen-size.service";
 import { SharedModule } from "../shared/shared.module";
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [NavComponent, NavbarComponent, NavbarComponent],
+  declarations: [NavbarComponent, NavbarComponent],
   imports: [CommonModule, RouterModule, SharedModule],
-  exports: [NavComponent, NavbarComponent, SharedModule],
+  exports: [NavbarComponent, SharedModule],
   providers: [ScreenSizeService]
 })
 export class CoreModule {}
